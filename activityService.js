75b53568133fd3297a8bfd9ef5ecb91e16d1159c
Troop 66 Webmaster <16478845+troop66matawan@users.noreply.github.com>
2020-10-04 12:43:10 -0400

    add convervation to service

--- activityService.js
+++ activityService.js
@@ -4,12 +4,19 @@ const ScoutbookBaseLog = require('./activityBase');
 class ScoutbookServiceLog extends ScoutbookBaseLog {
     constructor(date,hours,location,notes) {
         super(date,hours,location,notes);
+        this.conservation = false;
     }
     set hours(value){
         this.count = value;
     }
     get hours() {
         return this.count;
+    }
+    set conservation(value) {
+        this.conservation = value;
+    }
+    get conservation() {
+        return this.conservation;
     }
 }
 
